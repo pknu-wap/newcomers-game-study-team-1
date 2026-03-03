8c71aaab2c9006b75f831e46fddc5433131e7688
PermanentNostalgia <140535913+PermanentNostalgia@users.noreply.github.com>
2026-03-03 18:40:45 +0900

    WIP 레벨 시스템 구현
    # PR용 매모
    ## Rigidbody(강체)
    리지드바디는 외부의 충격에도 모양이 변하지 않는 물리적인 물체다.
    이를 게임상으로 구현하기 위해 Unity는 물리 시뮬레이션을 통해 오브젝트가 움직이도록 해주는 Rigidbody 컴포넌트를 지원한다.
    일반적으로, 오브젝트가 Rigidbody를 가지고 있으면 해당 오브젝트의 움직임은 물리 엔진에 의해 계산된다.
    
    Rigidbody2D는 이러한 Rigidbody의 일종으로 크게 3가지 모드가 존재한다.
    + Dynamic: 물리 법칙에 영향을 받아 움직임. 중력, 충돌에 따른 움직임을 구현할 때 주로 사용
    + Kinematic: 중력, 가해진 힘에 영향을 받지 않고 움직임. Script, 유저의 입력에 따라 움직임을 구현할 때 주로 사용
    + Static: 움직이지 않는 오브젝트에 적용
    
    Dynamic Rigidbody는 물리 연산이 적용된 움직임을 구현하기 위해 Rigidbody내 존재하는 메소드로 움직여야한다.
    2개의 Kinematic Rigidbody는 서로 물리적으로 충돌하지 않는다.
    
    ## Collider
    오브젝트의 영역을 지정하여 다른 오브젝트와의 물리적 충돌을 정의하기 위한 컴포넌트다.
    부모 오브젝트의 Rigidbody의 존재 유무에 따라 Static Collider(Rigidbody 없음), Rigidbody Collider(Rigidbody 있음)으로 나뉘어 불린다.
    Collider의 isTrigger 속성이 참이라면 해당 Collider는 다른 Collider와 물리적으로 충돌하지 않고 대신에 만나는 것을 감지한다.

--- Unity/Assets/Undead Survivor/Scripts/EnemyAttack.cs
+++ Unity/Assets/Undead Survivor/Scripts/EnemyAttack.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
     private float _damage = 1f;
-    private bool _isCooltime = false;
+    // private bool _isCooltime = false;
 
     private void OnTriggerStay2D(Collider2D other) {
         if(other.CompareTag("Player")) {
--- Unity/Assets/Undead Survivor/Scripts/FarmerLevel.cs
+++ Unity/Assets/Undead Survivor/Scripts/FarmerLevel.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class FarmerLevel : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private float _exp = 0;
+    private uint _level = 0;
+    private uint _maxExp = 25;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void AddExp(float exp) {
+        _exp += exp;
+        while(_exp >= _maxExp) {
+            _exp -= _maxExp;
+            _level++;
+            _maxExp = _level * 50;
+            Debug.Log(string.Format("레벨 업! {0}lv -> {1}lv", _level - 1, _level));
+        }
+
+        Debug.Log(string.Format("경험치: {0}, 레벨: {1}", _exp, _level));
     }
 }
