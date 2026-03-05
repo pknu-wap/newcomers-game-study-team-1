9f8acbdd1dab97c0803cd4c53eb66452a546ebc3
psm3489 <psm31560027@gmail.com>
2026-03-05 21:51:06 +0900

    유니티 레벨업화면(WIP)

--- Unity/Assets/Undead Survivor/Scripts/FarmerLevel.cs
+++ Unity/Assets/Undead Survivor/Scripts/FarmerLevel.cs
@@ -14,9 +14,9 @@ public class FarmerLevel : MonoBehaviour
             _currentExp -= _maxExp;
             _level++;
             _maxExp = _level * 50;
-            TimeController.PauseGame();
         }
         
         Debug.Log("레벨: " + _level + " 경험치: " + _currentExp);
+        TimeController.PauseGame();
     }
 }
