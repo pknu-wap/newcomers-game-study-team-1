51b8f77e43791b50af4d80d9311d20a56d236a2b
PermanentNostalgia <140535913+PermanentNostalgia@users.noreply.github.com>
2026-03-07 21:11:35 +0900

    WIP 청크 인덱스 벡터로 변환

--- Unity/Assets/Undead Survivor/Scripts/Core/TileGenerator.cs
+++ Unity/Assets/Undead Survivor/Scripts/Core/TileGenerator.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 using System.Collections.Generic;
 
 public class TileGenerator: MonoBehaviour
@@ -12,7 +13,7 @@ public class TileGenerator: MonoBehaviour
     public uint RenderDistance;
 
     private Vector2 _spawnPos;
-    private Dictionary<uint, Chunk> _chunks;
+    private Dictionary<uint, Chunk> _chunks = new();
     private uint _currentChunkIndex = 0;
 
     void Awake() {
@@ -28,6 +29,34 @@ public class TileGenerator: MonoBehaviour
     }
 
     void Update() {
-        
+
+    }
+
+    private Vector2 chunkIndexToVector2(uint index) {
+        uint layer;
+        if(index == 0) return new Vector2(0, 0);
+        else {
+            layer = ((uint)Math.Floor(Math.Sqrt((double)index)) + 1) / 2;
+        }
+
+        uint startIndex = (uint) Math.Pow(layer * 2 - 1, 2);
+        uint side = (index - startIndex) / (2*layer);
+        uint sideLength = 2 * layer + 1;
+        uint offsetInSide = (index - startIndex) % (2 * layer);
+
+        switch(side) {
+            case 0:
+                return new Vector2(offsetInSide + sideLength/2 - 2*layer + 1,sideLength / 2);
+            case 1:
+                return new Vector2(sideLength / 2, -offsetInSide + sideLength / 2);
+            case 2:
+                return new Vector2(-offsetInSide - sideLength/2 + 2*layer - 1 , -sideLength / 2);
+            case 3:
+                return new Vector2(-sideLength / 2, offsetInSide - sideLength / 2);
+        }
+    }
+
+    private void vector2ToChunkIndex() {
+
     }
 }
